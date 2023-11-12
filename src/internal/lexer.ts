687b4e4fb4182fee4971a82a6e5291b93f1002fc
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2023-11-11 18:34:42 -0800

    downgrade lexer

--- src/internal/lexer.ts
+++ src/internal/lexer.ts
@@ -2,36 +2,44 @@
 
 	markup/lexer.js
 
-	Copyright © 2013–2022 Thomas Michael Edwards <thomasmedwards@gmail.com>. All rights reserved.
+	Copyright © 2013–2021 Thomas Michael Edwards <thomasmedwards@gmail.com>. All rights reserved.
 	Use of this source code is governed by a BSD 2-clause "Simplified" License, which may be found in the LICENSE file.
 
 ***********************************************************************************************************************/
 
-// End of file (string, actually).
-const EOF = -1;
+export const {
+  // eslint-disable-line no-var
+  /* eslint-disable no-unused-vars */
+  EOF,
+  Lexer,
+  /* eslint-enable no-unused-vars */
+} = (() => {
+  'use strict';
 
-/*******************************************************************************
+  // End of file (string, actually).
+  const EOF = -1;
+
+  /*******************************************************************************************************************
 		Lexer Class.
-	*******************************************************************************/
+	*******************************************************************************************************************/
+  class Lexer {
+    declare source: string;
+    declare initial: any;
+    declare state: any;
+    declare start: any;
+    declare pos: number;
+    declare depth: number;
+    declare items: any[];
+    declare data: any;
 
-class Lexer {
-  declare source: string;
-  declare initial: any;
-  declare state: any;
-  declare start: any;
-  declare pos: number;
-  declare depth: number;
-  declare items: any[];
-  declare data: any;
+    constructor(source: string, initialState) {
+      if (arguments.length < 2) {
+        throw new Error(
+          'Lexer constructor called with too few parameters (source:string , initialState:function)'
+        );
+      }
 
-  constructor(source: string, initialState) {
-    if (arguments.length < 2) {
-      throw new Error(
-        'Lexer constructor called with too few parameters (source:string , initialState:function)'
-      );
-    }
-
-    /*
+      /*
 				this.source  → the string to be scanned
 				this.initial → initial state
 				this.state   → current state
@@ -41,216 +49,221 @@ class Lexer {
 				this.items   → scanned item queue
 				this.data    → lexing data
 			*/
-    Object.defineProperties(this, {
-      source: {
-        value: source,
-      },
+      Object.defineProperties(this, {
+        source: {
+          value: source,
+        },
 
-      initial: {
-        value: initialState,
-      },
+        initial: {
+          value: initialState,
+        },
 
-      state: {
-        writable: true,
-        value: initialState,
-      },
+        state: {
+          writable: true,
+          value: initialState,
+        },
 
-      start: {
-        writable: true,
-        value: 0,
-      },
+        start: {
+          writable: true,
+          value: 0,
+        },
 
-      pos: {
-        writable: true,
-        value: 0,
-      },
+        pos: {
+          writable: true,
+          value: 0,
+        },
 
-      depth: {
-        writable: true,
-        value: 0,
-      },
+        depth: {
+          writable: true,
+          value: 0,
+        },
 
-      items: {
-        writable: true,
-        value: [],
-      },
+        items: {
+          writable: true,
+          value: [],
+        },
 
-      data: {
-        writable: true,
-        value: {},
-      },
-    });
-  }
-
-  reset() {
-    this.state = this.initial;
-    this.start = 0;
-    this.pos = 0;
-    this.depth = 0;
-    this.items = [];
-    this.data = {};
-  }
-
-  run() {
-    // scan the source string until no states remain
-    while (this.state !== null) {
-      this.state = this.state(this);
+        data: {
+          writable: true,
+          value: {},
+        },
+      });
     }
 
-    // return the array of items
-    return this.items;
-  }
-
-  nextItem() {
-    // scan the source string until we have an item or no states remain
-    while (this.items.length === 0 && this.state !== null) {
-      this.state = this.state(this);
+    reset() {
+      this.state = this.initial;
+      this.start = 0;
+      this.pos = 0;
+      this.depth = 0;
+      this.items = [];
+      this.data = {};
     }
 
-    // return the current item
-    return this.items.shift();
-  }
+    run() {
+      // scan the source string until no states remain
+      while (this.state !== null) {
+        this.state = this.state(this);
+      }
 
-  next() {
-    if (this.pos >= this.source.length) {
-      return EOF;
+      // return the array of items
+      return this.items;
     }
 
-    return this.source[this.pos++];
-  }
+    nextItem() {
+      // scan the source string until we have an item or no states remain
+      while (this.items.length === 0 && this.state !== null) {
+        this.state = this.state(this);
+      }
 
-  peek() {
-    if (this.pos >= this.source.length) {
-      return EOF;
+      // return the current item
+      return this.items.shift();
     }
 
-    return this.source[this.pos];
-  }
+    next() {
+      if (this.pos >= this.source.length) {
+        return EOF;
+      }
 
-  backup(num?) {
-    // if (num) {
-    // 	this.pos -= num;
-    // }
-    // else {
-    // 	--this.pos;
-    // }
-    this.pos -= num || 1;
-  }
-
-  forward(num) {
-    // if (num) {
-    // 	this.pos += num;
-    // }
-    // else {
-    // 	++this.pos;
-    // }
-    this.pos += num || 1;
-  }
-
-  ignore() {
-    this.start = this.pos;
-  }
-
-  accept(valid) {
-    const ch = this.next();
-
-    if (ch === EOF) {
-      return false;
+      return this.source[this.pos++];
     }
 
-    if (valid.includes(ch)) {
-      return true;
+    peek() {
+      if (this.pos >= this.source.length) {
+        return EOF;
+      }
+
+      return this.source[this.pos];
     }
 
-    this.backup();
-    return false;
-  }
-
-  acceptRe(validRe) {
-    const ch = this.next();
-
-    if (ch === EOF) {
-      return false;
+    backup(num?: number) {
+      // if (num) {
+      // 	this.pos -= num;
+      // }
+      // else {
+      // 	--this.pos;
+      // }
+      this.pos -= num || 1;
     }
 
-    if (validRe.test(ch)) {
-      return true;
+    forward(num?: number) {
+      // if (num) {
+      // 	this.pos += num;
+      // }
+      // else {
+      // 	++this.pos;
+      // }
+      this.pos += num || 1;
     }
 
-    this.backup();
-    return false;
-  }
+    ignore() {
+      this.start = this.pos;
+    }
 
-  acceptRun(valid) {
-    for (;;) {
+    accept(valid) {
       const ch = this.next();
 
       if (ch === EOF) {
-        return;
+        return false;
       }
 
-      if (!valid.includes(ch)) {
-        break;
+      if (valid.includes(ch)) {
+        return true;
       }
+
+      this.backup();
+      return false;
     }
 
-    this.backup();
-  }
-
-  acceptRunRe(validRe) {
-    for (;;) {
+    acceptRe(validRe) {
       const ch = this.next();
 
       if (ch === EOF) {
-        return;
+        return false;
       }
 
-      if (!validRe.test(ch)) {
-        break;
+      if (validRe.test(ch)) {
+        return true;
       }
+
+      this.backup();
+      return false;
     }
 
-    this.backup();
-  }
+    acceptRun(valid) {
+      for (;;) {
+        const ch = this.next();
 
-  emit(type) {
-    this.items.push({
-      type,
-      text: this.source.slice(this.start, this.pos),
-      start: this.start,
-      pos: this.pos,
-    });
-    this.start = this.pos;
-  }
+        if (ch === EOF) {
+          return;
+        }
 
-  error(type, message) {
-    if (arguments.length < 2) {
-      throw new Error(
-        'Lexer.prototype.error called with too few parameters (type:number , message:string)'
-      );
+        if (!valid.includes(ch)) {
+          break;
+        }
+      }
+
+      this.backup();
     }
 
-    this.items.push({
-      type,
-      message,
-      text: this.source.slice(this.start, this.pos),
-      start: this.start,
-      pos: this.pos,
-    });
-    return null;
+    acceptRunRe(validRe) {
+      for (;;) {
+        const ch = this.next();
+
+        if (ch === EOF) {
+          return;
+        }
+
+        if (!validRe.test(ch)) {
+          break;
+        }
+      }
+
+      this.backup();
+    }
+
+    emit(type) {
+      this.items.push({
+        type,
+        text: this.source.slice(this.start, this.pos),
+        start: this.start,
+        pos: this.pos,
+      });
+      this.start = this.pos;
+    }
+
+    error(type, message) {
+      if (arguments.length < 2) {
+        throw new Error(
+          'Lexer.prototype.error called with too few parameters (type:number , message:string)'
+        );
+      }
+
+      this.items.push({
+        type,
+        message,
+        text: this.source.slice(this.start, this.pos),
+        start: this.start,
+        pos: this.pos,
+      });
+      return null;
+    }
+
+    static enumFromNames(names) {
+      const obj = names.reduce((obj, name, i) => {
+        obj[name] = i; // eslint-disable-line no-param-reassign
+        return obj;
+      }, {});
+      return Object.freeze(Object.assign(Object.create(null), obj));
+    }
   }
 
-  static enumFromNames(names) {
-    const obj = names.reduce((obj, name, i) => {
-      obj[name] = i; // eslint-disable-line no-param-reassign
-      return obj;
-    }, {});
-    return Object.freeze(Object.assign(Object.create(null), obj));
-  }
-}
+  /*******************************************************************************************************************
+		Module Exports.
+	*******************************************************************************************************************/
+  return {
+    EOF,
+    Lexer,
+  };
+})();
 
-/*******************************************************************************
-		Object Exports.
-	*******************************************************************************/
-
-export { EOF, Lexer };
+export type Lexer = InstanceType<typeof Lexer>;
