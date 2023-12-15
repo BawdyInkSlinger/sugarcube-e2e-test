c65dc846f486a1dd4a2f759016c87d3c740a10a3
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2023-12-14 18:25:17 -0800

    formatting and extracted debug-table.ts

---
+++ src/test-api/internal/inner-text/debug-table.ts
@@ -0,0 +1,5 @@
+export type DebugRow = {
+  functionName: string;
+  nodeInfo: string;
+  nodeText: string;
+};
--- src/test-api/internal/inner-text/inner-text-helper.ts
+++ src/test-api/internal/inner-text/inner-text-helper.ts
@@ -1,153 +1,152 @@
+import { DebugRow } from './debug-table';
+
 const nodeTypes = {
-    1: 'ELEMENT_NODE',
-    2: 'ATTRIBUTE_NODE',
-    3: 'TEXT_NODE',
-    4: 'CDATA_SECTION_NODE',
-    7: 'PROCESSING_INSTRUCTION_NODE',
-    8: 'COMMENT_NODE',
-    9: 'DOCUMENT_NODE',
-    10: 'DOCUMENT_TYPE_NODE',
-    11: 'DOCUMENT_FRAGMENT_NODE',
-  } as const;
-  
-  type NodeType = (typeof nodeTypes)[keyof typeof nodeTypes];
-  
-  type DebugRow = {
-    functionName: string;
-    nodeInfo: string;
-    nodeText: string;
-  };
+  1: 'ELEMENT_NODE',
+  2: 'ATTRIBUTE_NODE',
+  3: 'TEXT_NODE',
+  4: 'CDATA_SECTION_NODE',
+  7: 'PROCESSING_INSTRUCTION_NODE',
+  8: 'COMMENT_NODE',
+  9: 'DOCUMENT_NODE',
+  10: 'DOCUMENT_TYPE_NODE',
+  11: 'DOCUMENT_FRAGMENT_NODE',
+} as const;
+
+type NodeType = (typeof nodeTypes)[keyof typeof nodeTypes];
 
 export const innerTextHelper = (
-    el: Node
-  ): { result: string; debugDataTable: DebugRow[] } => {
-    const debugDataTable: DebugRow[] = [];
-  
-    const addToDebugDataTable = (
-      functionName: string,
-      nodeInfo: string,
-      nodeText: string
-    ): void => {
-      debugDataTable.push({ functionName, nodeInfo, nodeText });
-    };
-  
-    function handleText(node: Node, isPreviousElementInline: boolean, isNextElementInline: boolean): string {
-      let text = node.textContent.replaceAll(/\n/g, '').replaceAll(/ +/g, ' ');
-      text = isPreviousElementInline ? text : text.trimStart()
-      text = isNextElementInline ? text : text.trimEnd()
-  
-      addToDebugDataTable(handleText.name, node.nodeName, text);
-  
-      return text;
-    }
-  
-    function handleParentDiv(node: Node): string {
-      let recursed = innerTextHelper(node)
-        .result.replaceAll(/^\s+/g, '')
-        .replaceAll(/\s+$/g, '');
-      recursed = '\n' + recursed + '\n';
-  
-      addToDebugDataTable(
-        handleParentDiv.name,
-        `${node.nodeName}.${(node as HTMLElement).classList}`,
-        recursed
-      );
-  
-      return recursed;
-    }
-  
-    function handleHasChildNodes(node: Node): string {
-      const recursed = innerTextHelper(node).result;
-  
-      addToDebugDataTable(
-        handleHasChildNodes.name,
-        `${node.nodeName}.${(node as HTMLElement).classList}`,
-        recursed
-      );
-  
-      return recursed;
-    }
-  
-    function handleDoubleBr(node: Node): string {
-      const text = `\n\n`;
-  
-      addToDebugDataTable(
-        handleDoubleBr.name,
-        `${node.nodeName}.${(node as HTMLElement).classList}`,
-        text
-      );
-  
-      return text;
-    }
-  
-    function handleSingleBr(node: Node): string {
-      const text = ' ';
-  
-      addToDebugDataTable(
-        handleDoubleBr.name,
-        `${node.nodeName}.${(node as HTMLElement).classList}`,
-        text
-      );
-  
-      return text;
-    }
-  
-    function handleDefault(node: Node): string {
-      const text = '';
-  
-      addToDebugDataTable(
-        handleDefault.name,
-        `${node.nodeName}.${(node as HTMLElement).classList}`,
-        text
-      );
-  
-      return text;
-    }
-  
-    const result = [...el.childNodes]
-      .map((node, index, originalArray) => {
-        switch (getType(node)) {
-          case 'TEXT_NODE':
-            return handleText(
-              node,
-              index - 1 >= 0 &&
+  el: Node
+): { result: string; debugDataTable: DebugRow[] } => {
+  const debugDataTable: DebugRow[] = [];
+
+  const addToDebugDataTable = (
+    functionName: string,
+    nodeInfo: string,
+    nodeText: string
+  ): void => {
+    debugDataTable.push({ functionName, nodeInfo, nodeText });
+  };
+
+  function handleText(
+    node: Node,
+    isPreviousElementInline: boolean,
+    isNextElementInline: boolean
+  ): string {
+    let text = node.textContent.replaceAll(/\n/g, '').replaceAll(/ +/g, ' ');
+    text = isPreviousElementInline ? text : text.trimStart();
+    text = isNextElementInline ? text : text.trimEnd();
+
+    addToDebugDataTable(handleText.name, node.nodeName, text);
+
+    return text;
+  }
+
+  function handleParentDiv(node: Node): string {
+    let recursed = innerTextHelper(node)
+      .result.replaceAll(/^\s+/g, '')
+      .replaceAll(/\s+$/g, '');
+    recursed = '\n' + recursed + '\n';
+
+    addToDebugDataTable(
+      handleParentDiv.name,
+      `${node.nodeName}.${(node as HTMLElement).classList}`,
+      recursed
+    );
+
+    return recursed;
+  }
+
+  function handleHasChildNodes(node: Node): string {
+    const recursed = innerTextHelper(node).result;
+
+    addToDebugDataTable(
+      handleHasChildNodes.name,
+      `${node.nodeName}.${(node as HTMLElement).classList}`,
+      recursed
+    );
+
+    return recursed;
+  }
+
+  function handleDoubleBr(node: Node): string {
+    const text = `\n\n`;
+
+    addToDebugDataTable(
+      handleDoubleBr.name,
+      `${node.nodeName}.${(node as HTMLElement).classList}`,
+      text
+    );
+
+    return text;
+  }
+
+  function handleSingleBr(node: Node): string {
+    const text = ' ';
+
+    addToDebugDataTable(
+      handleDoubleBr.name,
+      `${node.nodeName}.${(node as HTMLElement).classList}`,
+      text
+    );
+
+    return text;
+  }
+
+  function handleDefault(node: Node): string {
+    const text = '';
+
+    addToDebugDataTable(
+      handleDefault.name,
+      `${node.nodeName}.${(node as HTMLElement).classList}`,
+      text
+    );
+
+    return text;
+  }
+
+  const result = [...el.childNodes]
+    .map((node, index, originalArray) => {
+      switch (getType(node)) {
+        case 'TEXT_NODE':
+          return handleText(
+            node,
+            index - 1 >= 0 &&
               originalArray[index - 1].nodeName.toLowerCase().trim() === `span`,
-              index + 1 < originalArray.length &&
-                originalArray[index + 1].nodeName.toLowerCase().trim() === `span`,
-            );
-          case 'ELEMENT_NODE':
-            if (
-              node.hasChildNodes() &&
-              node.nodeName.toLowerCase().trim() === `div`
-            ) {
-              return handleParentDiv(node);
-            } else if (node.hasChildNodes()) {
-              return handleHasChildNodes(node);
-            } else if (
-              node.nodeName.toLowerCase().trim() === `br` &&
-              index - 1 >= 0 &&
-              originalArray[index - 1].nodeName.toLowerCase().trim() === `br`
-            ) {
-              return handleDoubleBr(node);
-            } else if (
-              node.nodeName.toLowerCase().trim() === `br` &&
-              index - 1 >= 0 &&
-              originalArray[index - 1].nodeName.toLowerCase().trim() !== `br` &&
-              index + 1 < originalArray.length &&
-              originalArray[index + 1].nodeName.toLowerCase().trim() !== `br`
-            ) {
-              return handleSingleBr(node);
-            } else {
-              return handleDefault(node);
-            }
-        }
-      })
-      .join('');
-  
-    return { result: result.trim(), debugDataTable };
-  };
-  
-  const getType = (node: Node): NodeType => {
-    return nodeTypes[node.nodeType];
-  };
-  
+            index + 1 < originalArray.length &&
+              originalArray[index + 1].nodeName.toLowerCase().trim() === `span`
+          );
+        case 'ELEMENT_NODE':
+          if (
+            node.hasChildNodes() &&
+            node.nodeName.toLowerCase().trim() === `div`
+          ) {
+            return handleParentDiv(node);
+          } else if (node.hasChildNodes()) {
+            return handleHasChildNodes(node);
+          } else if (
+            node.nodeName.toLowerCase().trim() === `br` &&
+            index - 1 >= 0 &&
+            originalArray[index - 1].nodeName.toLowerCase().trim() === `br`
+          ) {
+            return handleDoubleBr(node);
+          } else if (
+            node.nodeName.toLowerCase().trim() === `br` &&
+            index - 1 >= 0 &&
+            originalArray[index - 1].nodeName.toLowerCase().trim() !== `br` &&
+            index + 1 < originalArray.length &&
+            originalArray[index + 1].nodeName.toLowerCase().trim() !== `br`
+          ) {
+            return handleSingleBr(node);
+          } else {
+            return handleDefault(node);
+          }
+      }
+    })
+    .join('');
+
+  return { result: result.trim(), debugDataTable };
+};
+
+const getType = (node: Node): NodeType => {
+  return nodeTypes[node.nodeType];
+};
