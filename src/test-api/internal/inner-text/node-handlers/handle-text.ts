d26783c6307d89b6c7fce5471a289ca438b00d3c
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2023-12-15 18:25:50 -0800

    extracted isInlineElementName

--- src/test-api/internal/inner-text/node-handlers/handle-text.ts
+++ src/test-api/internal/inner-text/node-handlers/handle-text.ts
@@ -6,15 +6,20 @@ export const handleText: NodeHandler = (
   originalArray: Node[]
 ): TextAndLog => {
   const isPreviousElementInline: boolean =
-    index - 1 >= 0 &&
-    originalArray[index - 1].nodeName.toLowerCase().trim() === `span`;
+    index - 1 >= 0 && isInlineElementName(originalArray[index - 1].nodeName);
   const isNextElementInline: boolean =
     index + 1 < originalArray.length &&
-    originalArray[index + 1].nodeName.toLowerCase().trim() === `span`;
+    isInlineElementName(originalArray[index + 1].nodeName);
 
   let text = node.textContent.replaceAll(/\n/g, '').replaceAll(/ +/g, ' ');
   text = isPreviousElementInline ? text : text.trimStart();
   text = isNextElementInline ? text : text.trimEnd();
 
   return returnWrapper(text, handleText.name, node.nodeName, text);
+};
+
+const isInlineElementName = (elementName: string): boolean => {
+  return [`span`].some((inlineElementName: string) => {
+    return inlineElementName === elementName.toLowerCase().trim();
+  });
 };
