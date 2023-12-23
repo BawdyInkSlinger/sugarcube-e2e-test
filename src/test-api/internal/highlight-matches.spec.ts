74863aa1573c1d209dca5c8d99d624debaf01968
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2023-12-22 21:11:49 -0800

    highlight matches works

--- src/test-api/internal/highlight-matches.spec.ts
+++ src/test-api/internal/highlight-matches.spec.ts
@@ -4,17 +4,48 @@ import chalk from 'chalk';
 
 describe(`highlightMatches`, () => {
   it('returns the original string if there are no matches', async () => {
-    expect(highlightMatches(splitMatches(`foo bar baz`, /qux/), chalk.bgGreen)).toEqual(
-      'foo bar baz'
-    );
+    expect(
+      highlightMatches(splitMatches(`foo bar baz`, /qux/), chalk.bgGreen)
+    ).toEqual('foo bar baz');
   });
-  
+
   it('returns a colored string if the whole string is a match', async () => {
-    expect(highlightMatches([{
-        value: 'foo bar baz',
-        isMatch: true
-    }], chalk.bgGreen)).toEqual(
-      chalk.bgGreen('foo bar baz')
-    );
+    expect(
+      highlightMatches(
+        [
+          {
+            value: 'foo bar baz',
+            isMatch: true,
+          },
+        ],
+        chalk.bgGreen
+      )
+    ).toEqual(chalk.bgGreen('foo bar baz'));
+  });
+
+  it('returns a colored string if part of the string is a match', async () => {
+    expect(
+      highlightMatches(
+        [
+          {
+            value: 'foo',
+            isMatch: true,
+          },
+          {
+            value: 'bar',
+            isMatch: false,
+          },
+          {
+            value: 'baz',
+            isMatch: true,
+          },
+          {
+            value: 'qux',
+            isMatch: false,
+          },
+        ],
+        chalk.bgRed
+      )
+    ).toEqual(`${chalk.bgRed(`foo`)}bar${chalk.bgRed(`baz`)}qux`);
   });
 });
