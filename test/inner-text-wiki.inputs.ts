0140fb75b12da72aaf1f3aa3d03f595e10685ad1
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2024-01-29 16:45:16 -0800

    Added a working test; the only difference being temp var vs hardcoded

--- test/inner-text-wiki.inputs.ts
+++ test/inner-text-wiki.inputs.ts
@@ -1,5 +1,20 @@
-export const expected1 = `Author: Test2`;
+export const expected1 = `Author: Test1`;
 export const wiki1 = `    <div class="author">
+      Author: 
+      <<if _author_href is undefined>>
+        Test1
+      <<else>>
+        <a @href="_author_href">_author</a>
+      <</if>>
+    </div>`;
+export const passageTags1 = [`nobr`];
+export const state1 = {
+  variables: {},
+  temporary: {},
+};
+
+export const expected2 = `Author: Test2`;
+export const wiki2 = `    <div class="author">
       Author: 
       <<if _author_href is undefined>>
         _author
@@ -7,8 +22,8 @@ export const wiki1 = `    <div class="author">
         <a @href="_author_href">_author</a>
       <</if>>
     </div>`;
-export const passageTags1 = [`nobr`];
-export const state1 = {
+export const passageTags2 = [`nobr`];
+export const state2 = {
   variables: {},
   temporary: {
     'author': 'Test2',
