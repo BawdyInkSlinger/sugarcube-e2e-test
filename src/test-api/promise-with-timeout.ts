1741a73524484fb6bd6ffa4094eadfacf62127b0
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2024-05-27 15:36:19 -0700

    Create PromiseTimeoutError. Next, add more constructors

--- src/test-api/promise-with-timeout.ts
+++ src/test-api/promise-with-timeout.ts
@@ -11,7 +11,7 @@ export async function promiseWithTimeout<PR>(
     logger.debug(`entering timeout promise. millis='${millis}'`);
     timeoutPid = setTimeout(() => {
       logger.debug(`rejecting timeout promise`);
-      reject(new Error(`Timed out after ${millis} ms.`));
+      reject(new PromiseTimeoutError(millis));
     }, millis);
   });
   return Promise.race([promise, timeout]).finally(() => {
@@ -22,4 +22,21 @@ export async function promiseWithTimeout<PR>(
       clearTimeout(timeoutPid);
     }
   });
+}
+
+export class PromiseTimeoutError extends Error {
+  // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#custom_error_types
+  timeoutMillis: number;
+
+  constructor(timeoutMillis: number, message?: string, options?: ErrorOptions) {
+    super(message ?? `Timed out after ${timeoutMillis} ms.`, options);
+
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, PromiseTimeoutError);
+    }
+
+    this.name = 'PromiseTimeoutError';
+    this.timeoutMillis = timeoutMillis;
+  }
 }
