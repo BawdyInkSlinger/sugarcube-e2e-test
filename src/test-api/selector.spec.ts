c8d0b1bde6f6572255762eb9bb6a41957957dced
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2024-06-16 21:13:04 -0700

    Move selector withText/withExactText tests into their own file

--- src/test-api/selector.spec.ts
+++ src/test-api/selector.spec.ts
@@ -45,31 +45,6 @@ describe(`selector`, () => {
       .eql(`Button 3`);
   });
 
-  it('can click a button withText', async () => {
-    const sugarcubeParser = await SugarcubeParser.create({
-      passages: [
-        {
-          title: 'passage title',
-          tags: ['passage tag'],
-          text: '<button disabled>Button 1</button> <<button "Button 2" "passage 2">><</button>> <button disabled>Button 3</button>',
-        },
-        {
-          title: 'passage 2',
-          tags: ['passage tag'],
-          text: 'Destination',
-        },
-      ],
-    });
-
-    await sugarcubeParser.testController
-      .goto('passage title')
-      .click(Selector('.passage button').withText(`Button 2`), {
-        waitFor: 'click end',
-      })
-      .expect(Selector('.passage').innerText)
-      .eql(`Destination`);
-  });
-
   it('can click a button that stays on the current passage', async () => {
     const sugarcubeParser = await SugarcubeParser.create({
       passages: [
@@ -335,69 +310,5 @@ describe(`selector`, () => {
       .goto('passage title')
       .expect(reExecutablePromise)
       .eql(` <p> Words words words <span> nest </span> </p> `);
-  });
-
-  it('has withExactText', async () => {
-    const sugarcubeParser = await SugarcubeParser.create({
-      passages: [
-        {
-          title: 'passage title',
-          tags: ['passage tag'],
-          text: `<button disabled>Button A</button> <<button "Button ZA">><</button>> <button disabled>Button ABC</button> <button>Button A</button>`,
-        },
-      ],
-    });
-
-    const reExecutablePromise =
-      Selector('button').withExactText(`Button A`).count;
-
-    await sugarcubeParser.testController
-      .goto('passage title')
-      .expect(Selector('button').withText(`Button A`).count)
-      .eql(3)
-      .expect(reExecutablePromise)
-      .eql(2);
-  });
-
-  it('has withExactText with complex selector', async () => {
-    const sugarcubeParser = await SugarcubeParser.create({
-      passages: [
-        {
-          title: 'passage title',
-          tags: ['passage tag'],
-          text: `<div id="outer"><div class="inner"><button disabled>Button A</button> <<button "Button ZA">><</button>> <button disabled>Button ABC</button> <button>Button A</button></div></div>`,
-        },
-      ],
-    });
-
-    const reExecutablePromise = Selector('#outer .inner button').withExactText(
-      `Button A`
-    ).count;
-
-    await sugarcubeParser.testController
-      .goto('passage title')
-      .expect(Selector('#outer .inner button').withText(`Button A`).count)
-      .eql(3)
-      .expect(reExecutablePromise)
-      .eql(2);
-  });
-  
-  it('counts as "exact text" when the exact text is nested within child elements', async () => {
-    const sugarcubeParser = await SugarcubeParser.create({
-      passages: [
-        {
-          title: 'passage title',
-          tags: ['passage tag'],
-          text: `<div id="nested-one-child"><span>foobar</span></div>`,
-        },
-      ],
-    });
-
-    await sugarcubeParser.testController
-      .goto('passage title')
-      .expect(Selector('#nested-one-child').withExactText(
-        `foobar`
-      ).exists)
-      .ok()
   });
 });
