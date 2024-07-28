c58c5578622ff4d46f1d6d78f341f6f702568693
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2024-07-27 19:55:57 -0700

    Wrote a failing test

--- src/internal/history.spec.ts
+++ src/internal/history.spec.ts
@@ -2,7 +2,58 @@ import { SugarcubeParser } from '../sugarcube-parser';
 import { Selector } from '../test-api/selector';
 
 describe(`History`, () => {
-  it('can go backward and forward in history', async () => {
+  async function forwardBackwardTest(sugarcubeParser: SugarcubeParser) {
+    await sugarcubeParser.testController
+      .goto('passage title 1')
+      .expect(Selector(`.passage`).innerText)
+      .contains(`page 1`)
+      .expect(Selector(`#history-backward:enabled`).exists)
+      .notOk()
+      .expect(Selector(`#history-forward:enabled`).exists)
+      .notOk()
+      // Go to the second page:
+      .click(Selector(`button`).withText(`passage title 2`))
+      .expect(Selector(`.passage`).innerText)
+      .contains(`page 2`)
+      .expect(Selector(`#history-backward:enabled`).exists)
+      .ok()
+      .expect(Selector(`#history-forward:enabled`).exists)
+      .notOk()
+      // Go to the third page:
+      .click(Selector(`button`).withText(`passage title 3`))
+      .expect(Selector(`.passage`).innerText)
+      .contains(`page 3`)
+      .expect(Selector(`#history-backward:enabled`).exists)
+      .ok()
+      .expect(Selector(`#history-forward:enabled`).exists)
+      .notOk()
+      // Go back
+      .click(Selector('#history-backward'))
+      .expect(Selector(`.passage`).innerText)
+      .contains(`page 2`)
+      .expect(Selector(`#history-backward:enabled`).exists)
+      .ok()
+      .expect(Selector(`#history-forward:enabled`).exists)
+      .ok()
+      // Go back
+      .click(Selector('#history-backward'))
+      .expect(Selector(`.passage`).innerText)
+      .contains(`page 1`)
+      .expect(Selector(`#history-backward:enabled`).exists)
+      .notOk()
+      .expect(Selector(`#history-forward:enabled`).exists)
+      .ok()
+      // Go forward
+      .click(Selector('#history-forward'))
+      .expect(Selector(`.passage`).innerText)
+      .contains(`page 2`)
+      .expect(Selector(`#history-backward:enabled`).exists)
+      .ok()
+      .expect(Selector(`#history-forward:enabled`).exists)
+      .ok();
+  }
+
+  it('can go backward and forward with a reused parser', async () => {
     const passages = [
       {
         title: 'passage title 1',
@@ -24,28 +75,68 @@ describe(`History`, () => {
     const sugarcubeParser = await SugarcubeParser.create({
       passages,
     });
+    await forwardBackwardTest(sugarcubeParser);
 
-    await sugarcubeParser.testController
-      .goto('passage title 1')
-      .expect(Selector(`.passage`).innerText)
-      .contains(`page 1`)
-      // Go to the second page:
-      .click(Selector(`button`).withText(`passage title 2`))
-      .expect(Selector(`.passage`).innerText)
-      .contains(`page 2`)
-      // Go to the third page:
-      .click(Selector(`button`).withText(`passage title 3`))
-      .expect(Selector(`.passage`).innerText)
-      .contains(`page 3`)
-      // Go back twice
-      .logDocument({includeHeadElement: false})
-      .click(Selector('#history-backward'))
-      .click(Selector('#history-backward'))
-      .expect(Selector(`.passage`).innerText)
-      .contains(`page 1`)
-      // Go forward once
-      .click(Selector('#history-forward'))
-      .expect(Selector(`.passage`).innerText)
-      .contains(`page 2`)
+    await sugarcubeParser.assignStateAndReload({});
+    await forwardBackwardTest(sugarcubeParser);
+  });
+
+  xit('can go backward and forward with a custom StoryInterface', async () => {
+    const passageEndHandler = `
+$(document).on(':passageend', function (ev) {
+  $(\`#history-backward\`)
+    .ariaClick({ label: 'Go backward within the game history' }, () => {
+      Engine.backward();
+    })
+    .ariaDisabled(State.length < 2)
+    .ariaIsDisabled() &&
+    $(\`#history-backward\`).prop('disabled', true);
+
+  $(\`#history-forward\`)
+    .ariaClick({ label: 'Go forward within the game history' }, () => {
+      Engine.forward();
+    })
+    .ariaDisabled(State.length === State.size)
+    .ariaIsDisabled() &&
+    $(\`#history-forward\`).prop('disabled', true);
+});
+    `;
+    const passages = [
+      {
+        title: 'passage title 1',
+        tags: ['passage tag 1'],
+        text: 'page 1 <<button "passage title 2" "passage title 2">><</button>>',
+      },
+      {
+        title: 'passage title 2',
+        tags: ['passage tag 2'],
+        text: 'page 2 <<button "passage title 3" "passage title 3">><</button>>',
+      },
+      {
+        title: 'passage title 3',
+        tags: ['passage tag 3'],
+        text: 'page 3',
+      },
+      {
+        title: 'StoryInterface',
+        tags: [],
+        text: `
+        <button id="history-backward" class="history__button menu__button">Backward</button>
+        <button id="history-forward" class="history__button menu__button">Forward</button>
+        <div id="passages"></div>
+        `,
+      },
+      {
+        title: 'Script',
+        tags: [`script`],
+        text: passageEndHandler,
+      },
+    ];
+
+    const sugarcubeParser = await SugarcubeParser.create({
+      passages,
+    });
+
+    await forwardBackwardTest(sugarcubeParser);
   });
 });
