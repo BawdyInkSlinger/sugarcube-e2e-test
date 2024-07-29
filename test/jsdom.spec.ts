5caffc02d470a438c813756c343400c24bec2995
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2024-07-28 22:12:31 -0700

    Exploratory test for JSDOM's window.getComputedStyle

--- test/jsdom.spec.ts
+++ test/jsdom.spec.ts
@@ -19,4 +19,76 @@ describe(`jsdom`, () => {
       expect(window.requestAnimationFrame).toBeUndefined();
     });
   });
+
+  fdescribe(`getStyleProperty`, () => {
+    it(`can compute default property values`, async () => {
+      const { window } = new JSDOM('<p>Hello!</p>', {
+        pretendToBeVisual: true,
+      });
+
+      expect(
+        window.getComputedStyle(window.document.querySelector(`p`)!)['display']
+      ).toEqual('block');
+    });
+    
+    it(`can compute inline property styles`, async () => {
+      const { window } = new JSDOM('<p style="display: none">Hello!</p>', {
+        pretendToBeVisual: true,
+      });
+
+      expect(
+        window.getComputedStyle(window.document.querySelector(`p`)!)['display']
+      ).toEqual('none');
+    });
+    
+    it(`can compute head element styles`, async () => {
+      const { window } = new JSDOM(`
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <style>
+        p {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+    <p>Hello!</p>
+</body>
+</html>
+        `, {
+        pretendToBeVisual: true,
+      });
+
+      expect(
+        window.getComputedStyle(window.document.querySelector(`p`)!)['display']
+      ).toEqual('none');
+    });
+    
+    it(`can compute styles from CSS class`, async () => {
+      const { window } = new JSDOM(`
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <style>
+        .my-prop {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+    <p class="my-prop">Hello!</p>
+</body>
+</html>
+        `, {
+        pretendToBeVisual: true,
+      });
+
+      expect(
+        window.getComputedStyle(window.document.querySelector(`p`)!)['display']
+      ).toEqual('none');
+    });
+  });
 });
