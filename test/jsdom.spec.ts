098310e9cb30db651780d09c13cf464fa671409b
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2024-07-28 17:29:44 -0700

    Refactor: put tests in a nested describe

--- test/jsdom.spec.ts
+++ test/jsdom.spec.ts
@@ -1,20 +1,22 @@
-import './../src/test-api/internal/monkey-patching/jsdom/strings'
+import './../src/test-api/internal/monkey-patching/jsdom/strings';
 import { JSDOM } from 'jsdom';
 
 describe(`jsdom`, () => {
-  it(`has window.requestAnimationFrame when pretendToBeVisual is true`, async () => {
-    const { window } = new JSDOM('', {
-      pretendToBeVisual: true,
+  describe(`requestAnimationFrame`, () => {
+    it(`has window.requestAnimationFrame when pretendToBeVisual is true`, async () => {
+      const { window } = new JSDOM('', {
+        pretendToBeVisual: true,
+      });
+
+      expect(window.requestAnimationFrame).not.toBeUndefined();
     });
 
-    expect(window.requestAnimationFrame).not.toBeUndefined();
-  });
+    it('does not have window.requestAnimationFrame when pretendToBeVisual is false', async () => {
+      const { window } = new JSDOM('', {
+        pretendToBeVisual: false,
+      });
 
-  it('does not have window.requestAnimationFrame when pretendToBeVisual is false', async () => {
-    const { window } = new JSDOM('', {
-      pretendToBeVisual: false,
+      expect(window.requestAnimationFrame).toBeUndefined();
     });
-
-    expect(window.requestAnimationFrame).toBeUndefined();
   });
 });
