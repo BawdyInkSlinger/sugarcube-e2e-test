1f0e612f0c1d36ccb03386666dfe09c2a5ddc5cd
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2024-08-03 22:36:26 -0700

    Passage compiles, but nothing can use it. Checkpoint

--- src/internal/fakes/passage.ts
+++ src/internal/fakes/passage.ts
@@ -1,13 +1,26 @@
-import { Config } from '../config';
-import { SimplePassage } from '../declarations/unofficial/simple-passage';
-import { State } from '../state';
-import { Wikifier } from '../wikifier';
+/***********************************************************************************************************************
+
+	passage.js
+
+	Copyright © 2013–2021 Thomas Michael Edwards <thomasmedwards@gmail.com>. All rights reserved.
+	Use of this source code is governed by a BSD 2-clause "Simplified" License, which may be found in the LICENSE file.
+
+***********************************************************************************************************************/
+/* global Config, L10n, Util, Wikifier */
+
 import { TWINE1 } from '../../constants';
-import { Util } from '../util';
+import { getLogger } from '../../logging/logger';
+import { Config } from '../config';
 import { L10n } from '../l10n';
+import { Util } from '../util';
+import { Wikifier } from '../wikifier';
+
+const logger = getLogger();
 
 export const PassageClass = (() => {
   // eslint-disable-line no-unused-vars, no-var
+  'use strict';
+
   let _tagsToSkip;
   let _twine1Unescape;
 
@@ -38,7 +51,7 @@ export const PassageClass = (() => {
 		*/
     const _twine1EscapesRe = /(?:\\n|\\t|\\s|\\|\r)/g;
     const _hasTwine1EscapesRe = new RegExp(_twine1EscapesRe.source); // to drop the global flag
-    const _twine1EscapesMap = Util.toEnum({
+    const _twine1EscapesMap = Object.freeze({
       '\\n': '\n',
       '\\t': '\t',
       '\\s': '\\',
@@ -59,72 +72,73 @@ export const PassageClass = (() => {
     };
   }
 
-  /*******************************************************************************
+  /*******************************************************************************************************************
 		Passage Class.
-	*******************************************************************************/
-
-  class PassageClass {
+	*******************************************************************************************************************/
+  class Passage {
     declare id: string;
     declare domId: string;
-    declare name: string;
     declare title: string;
-    declare text: string;
+    // declare text: string;
     declare tags: string[];
     declare classes: string[];
+    declare element: Element | null;
+    declare _excerpt: string | null;
 
-    constructor({ title, text, tags }: SimplePassage) {
-      const sortedAndUniqueTags = [...new Set<string>(tags)].sort((a, b) =>
-        a.localeCompare(b)
-      );
-
-      const id = `passage-${Util.slugify(title)}`;
-      const name = Util.entityDecode(title);
-      this.#construct(id, name, text, sortedAndUniqueTags);
-    }
-
-    // I extracted this private method to rename sortedAndUniqueTags -> tags and prevent the possibility of unsorted tag being used accidentally
-    #construct(id: string, name: string, text: string, tags: string[]) {
+    constructor(title: string, el?: Element) {
       Object.defineProperties(this, {
-        // Passage title.
-        name: {
-          value: name,
+        // Passage title/ID.
+        title: {
+          value: Util.unescape(title),
         },
 
-        text: {
-          value: text,
+        // Passage data element (within the story data element; i.e. T1: '[tiddler]', T2: 'tw-passagedata').
+        element: {
+          value: el || null,
         },
 
         // Passage tags array (unique).
         tags: {
-          value: tags,
+          value: Object.freeze(
+            el && el.hasAttribute('tags')
+              ? Array.from(
+                  new Set(el.getAttribute('tags').trim().splitOrEmpty(/\s+/))
+                )
+              : []
+          ),
         },
 
+        // Passage excerpt.  Used by the `description()` method.
+        _excerpt: {
+          writable: true,
+          value: null,
+        },
+      });
+
+      // Properties dependant upon the above set.
+      Object.defineProperties(this, {
         // Passage DOM-compatible ID.
-        id: {
-          value: id,
+        domId: {
+          value: `passage-${Util.slugify(this.title)}`,
         },
 
         // Passage classes array (sorted and unique).
         classes: {
           value: Object.freeze(
-            tags.length === 0
+            this.tags.length === 0
               ? []
               : (() =>
-                  // Return the sorted list of unique classes.
-                  tags
+                  /*
+							Return the sorted list of unique classes.
+
+							NOTE: The `this.tags` array is already sorted and unique,
+							so we only need to filter and map here.
+						*/
+                  this.tags
                     .filter((tag) => !_tagsToSkip.test(tag))
                     .map((tag) => Util.slugify(tag)))()
           ),
         },
-
-        /* legacy */
-        domId: {
-          value: id,
-        },
-        title: {
-          value: name,
-        },
-        /* /legacy */
       });
     }
 
@@ -133,8 +147,71 @@ export const PassageClass = (() => {
       return this.classes.join(' ');
     }
 
+    // TODO: (v3) This should be → `get source`.
+    get text() {
+      if (this.element == null) {
+        // lazy equality for null
+        const passage = Util.escapeMarkup(this.title);
+        const mesg = `${L10n.get('errorTitle')}: ${L10n.get(
+          'errorNonexistentPassage',
+          { passage }
+        )}`;
+        return `<div class="error-view"><span class="error">${mesg}</span></div>`;
+      }
+
+      // For Twine 1
+      if (TWINE1) {
+        return _twine1Unescape(this.element.textContent);
+      }
+      // For Twine 2
+      else {
+        // eslint-disable-line no-else-return
+        return this.element.textContent.replace(/\r/g, '');
+      }
+    }
+
+    description() {
+      const descriptions = Config.passages.descriptions;
+
+      switch (typeof descriptions) {
+        case 'boolean':
+          if (descriptions) {
+            return this.title;
+          }
+          break;
+
+        case 'object':
+          if (descriptions.hasOwnProperty(this.title)) {
+            return descriptions[this.title];
+          }
+          break;
+
+        case 'function':
+          {
+            const result = descriptions.call(this);
+
+            if (result) {
+              return result;
+            }
+          }
+          break;
+      }
+
+      // Initialize the excerpt cache from the raw passage text, if necessary.
+      if (this._excerpt === null) {
+        this._excerpt = Passage.getExcerptFromText(this.text);
+      }
+
+      return this._excerpt;
+    }
+
     // TODO: (v3) This should be → `get text`.
     processText() {
+      if (this.element == null) {
+        // lazy equality for null
+        return this.text;
+      }
+
       // Handle image passage transclusion.
       if (this.tags.includes('Twine.image')) {
         return `[img[${this.text}]]`;
@@ -145,7 +222,7 @@ export const PassageClass = (() => {
       // Handle `Config.passages.onProcess`.
       if (Config.passages.onProcess) {
         processed = Config.passages.onProcess.call(null, {
-          title: this.name,
+          title: this.title,
           tags: this.tags,
           text: processed,
         });
@@ -161,25 +238,94 @@ export const PassageClass = (() => {
       return processed;
     }
 
-    render(options?) {
+    render(options) {
+      // Wikify the passage into a document fragment.
       const frag = document.createDocumentFragment();
       new Wikifier(frag, this.processText(), options);
+
+      // Update the excerpt cache to reflect the rendered text.
+      this._excerpt = Passage.getExcerptFromNode(frag);
+
       return frag;
     }
 
-    /* legacy */
-    description() {
-      // eslint-disable-line class-methods-use-this
-      return `${L10n.get('turn')} ${State.turns}`;
+    static getExcerptFromNode(node: DocumentFragment, count?) {
+      logger.debug(
+        `[Passage.getExcerptFromNode(node=…, count=${count})]`,
+        node
+      );
+
+      if (!node.hasChildNodes()) {
+        return '';
+      }
+
+      // WARNING: es5-shim's `<String>.trim()` can cause "too much recursion" errors
+      // here on very large strings (e.g., ≥40 KiB), at least in Firefox, for unknown
+      // reasons.
+      //
+      // To fix the issue, we're removed `\u180E` from es5-shim's whitespace pattern
+      // to prevent it from erroneously shimming `<String>.trim()` in the first place.
+      let excerpt = node.textContent.trim();
+
+      if (excerpt !== '') {
+        const excerptRe = new RegExp(
+          `(\\S+(?:\\s+\\S+){0,${count > 0 ? count - 1 : 7}})`
+        );
+        // @ts-ignore
+        excerpt = excerpt
+          // Compact whitespace.
+          .replace(/\s+/g, ' ')
+          // Attempt to match the excerpt regexp.
+          .match(excerptRe);
+      }
+
+      return excerpt ? `${excerpt[1]}\u2026` : '\u2026'; // horizontal ellipsis
     }
-    /* /legacy */
+
+    static getExcerptFromText(text: string, count?) {
+      logger.debug(
+        `[Passage.getExcerptFromText(text=…, count=${count})]`,
+        text
+      );
+
+      if (text === '') {
+        return '';
+      }
+
+      const excerptRe = new RegExp(
+        `(\\S+(?:\\s+\\S+){0,${count > 0 ? count - 1 : 7}})`
+      );
+      const excerpt = text
+        // Strip macro tags (replace with a space).
+        .replace(/<<.*?>>/g, ' ')
+        // Strip html tags (replace with a space).
+        .replace(/<.*?>/g, ' ')
+        // The above might have left problematic whitespace, so trim.
+        .trim()
+        // Strip table markup.
+        .replace(/^\s*\|.*\|.*?$/gm, '')
+        // Strip image markup.
+        .replace(/\[[<>]?img\[[^\]]*\]\]/g, '')
+        // Clean link markup (remove all but the link text).
+        .replace(/\[\[([^|\]]*?)(?:(?:\||->|<-)[^\]]*)?\]\]/g, '$1')
+        // Clean heading markup.
+        .replace(/^\s*!+(.*?)$/gm, '$1')
+        // Clean bold/italic/underline/highlight styles.
+        .replace(/'{2}|\/{2}|_{2}|@{2}/g, '')
+        // A final trim.
+        .trim()
+        // Compact whitespace.
+        .replace(/\s+/g, ' ')
+        // Attempt to match the excerpt regexp.
+        .match(excerptRe);
+      return excerpt ? `${excerpt[1]}\u2026` : '\u2026'; // horizontal ellipsis
+    }
   }
 
-  /*******************************************************************************
-		Object Exports.
-	*******************************************************************************/
-
-  return PassageClass;
+  /*******************************************************************************************************************
+		Module Exports.
+	*******************************************************************************************************************/
+  return Passage;
 })();
 
 export type Passage = InstanceType<typeof PassageClass>;
