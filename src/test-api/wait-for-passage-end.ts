1389108d683d88ca7113a95e610efb356a5c45a3
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2024-08-04 18:53:11 -0700

    Refactor waitForPassageEnd to async via VSC Quick Fix

--- src/test-api/wait-for-passage-end.ts
+++ src/test-api/wait-for-passage-end.ts
@@ -6,23 +6,22 @@ import {
 const logger = getLogger('DEFAULT');
 const enterLogger = getLogger('DEBUG_TEST_CONTROLLER_ENTER_LOG_MESSAGES');
 
-export function waitForPassageEnd(debugNote = '', timeoutMillis = 2000) {
+export async function waitForPassageEnd(debugNote = '', timeoutMillis = 2000) {
   enterLogger.debug(
     `waitForPassageEnd: entering waitForPassageEnd debugNote=${debugNote}`
   );
-  return promiseWithTimeout(
-    timeoutMillis,
-    new Promise<void>((resolve) => {
-      $(document).one(':passageend', function () {
-        logger.debug(
-          `waitForPassageEnd: resolving waitForPassageEnd :passageend debugNote=${debugNote}`
-        );
-        resolve();
-      });
-    })
-  ).then(() => {
-    logger.debug(
-      `waitForPassageEnd: resolving waitForPassageEnd then debugNote=${debugNote}`
+  await promiseWithTimeout(
+        timeoutMillis,
+        new Promise<void>((resolve) => {
+            $(document).one(':passageend', function () {
+                logger.debug(
+                    `waitForPassageEnd: resolving waitForPassageEnd :passageend debugNote=${debugNote}`
+                );
+                resolve();
+            });
+        })
     );
-  });
+    logger.debug(
+        `waitForPassageEnd: resolving waitForPassageEnd then debugNote=${debugNote}`
+    );
 }
