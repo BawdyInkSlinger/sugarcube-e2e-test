8387035f7ffa4bca8ba49ad37b239576199271d8
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2024-08-13 14:20:39 -0700

    Lets you clear timeouts

--- src/observable-timeout.spec.ts
+++ src/observable-timeout.spec.ts
@@ -1,5 +1,5 @@
 import './test-api/internal/monkey-patching/jsdom/strings';
-import { getObservableTimeouts, observeTimeout } from './observable-timeout';
+import { clearTimeouts, getObservableTimeouts, observeTimeout } from './observable-timeout';
 import {
   ResourceLoaderConstructorOptions,
   ResourceLoader,
@@ -14,6 +14,24 @@ describe('Observable Timeout', () => {
     observeTimeout('c', () => {}, 3);
 
     await Promise.all(getObservableTimeouts());
+
+    expect(getObservableTimeouts()).toEqual([]);
+  });
+
+  it('contains timers if you do not Promise.all', async () => {
+    observeTimeout('a', () => {}, 1);
+    observeTimeout('b', () => {}, 2);
+    observeTimeout('c', () => {}, 3);
+
+    expect(getObservableTimeouts().length).not.toEqual(0);
+  });
+
+  it('lets you clear timeouts', async () => {
+    observeTimeout('a', () => {}, 1);
+    observeTimeout('b', () => {}, 2);
+    observeTimeout('c', () => {}, 3);
+
+    await clearTimeouts()
 
     expect(getObservableTimeouts()).toEqual([]);
   });
