e5e3f5396427458b24b6eeb772074ebfa2825890
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2024-11-02 21:18:52 -0700

    Remove NodeSnapshot functions I don't need to implement

--- src/test-api/html-element-to-node-snapshot.ts
+++ src/test-api/html-element-to-node-snapshot.ts
@@ -1,11 +1,7 @@
 import { NodeSnapshot } from './internal/node-snapshot';
-import { TextRectangle } from './internal/text-rectangle';
 
 export const htmlElementToNodeSnapshot = (el: HTMLElement): NodeSnapshot => {
-  return {
-    get childElementCount(): number {
-      throw new Error(`Not Implemented`);
-    },
+  const result: NodeSnapshot= Object.assign({}, el, {
     get childNodeCount(): number {
       throw new Error(`Not Implemented`);
     },
@@ -15,104 +11,13 @@ export const htmlElementToNodeSnapshot = (el: HTMLElement): NodeSnapshot => {
     get hasChildNodes(): boolean {
       throw new Error(`Not Implemented`);
     },
-    get nodeType(): number {
-      throw new Error(`Not Implemented`);
-    },
-    get textContent(): string {
-      throw new Error(`Not Implemented`);
-    },
     get attributes(): { [name: string]: string } | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get boundingClientRect(): TextRectangle | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get checked(): boolean | undefined | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get classNames(): string[] | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get clientHeight(): number | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get clientLeft(): number | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get clientTop(): number | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get clientWidth(): number | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get focused(): boolean | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get id(): string | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get innerText(): string | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get namespaceURI(): string | null | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get offsetHeight(): number | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get offsetLeft(): number | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get offsetTop(): number | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get offsetWidth(): number | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get selected(): boolean | undefined | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get selectedIndex(): number | undefined | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get scrollHeight(): number | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get scrollLeft(): number | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get scrollTop(): number | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get scrollWidth(): number | undefined {
       throw new Error(`Not Implemented`);
     },
     get style(): { [prop: string]: string } | undefined {
       throw new Error(`Not Implemented`);
     },
-    get tagName(): string | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get value(): string | undefined | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    get visible(): boolean | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    hasClass(className: string): boolean | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    getStyleProperty(propertyName: string): string | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    getAttribute(attributeName: string): string | null | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    getBoundingClientRectProperty(propertyName: string): number | undefined {
-      throw new Error(`Not Implemented`);
-    },
-    hasAttribute(attributeName: string): boolean | undefined {
-      throw new Error(`Not Implemented`);
-    },
-  };
+  });
+
+  return result;
 };
