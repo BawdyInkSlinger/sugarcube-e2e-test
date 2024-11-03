2a676c280dc4ef1f212e2acce3c0042aa35d9cba
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2024-11-02 21:14:04 -0700

    htmlElementToNodeSnapshot compiles

--- src/test-api/html-element-to-node-snapshot.ts
+++ src/test-api/html-element-to-node-snapshot.ts
@@ -1,46 +1,118 @@
-import { NodeSnapshot } from './internal/node-snapshot'
+import { NodeSnapshot } from './internal/node-snapshot';
+import { TextRectangle } from './internal/text-rectangle';
 
 export const htmlElementToNodeSnapshot = (el: HTMLElement): NodeSnapshot => {
   return {
- 
-  childElementCount: number;
-  childNodeCount: number;
-  hasChildElements: boolean;
-  hasChildNodes: boolean;
-  nodeType: number;
-  textContent: string;
-  attributes?: { [name: string]: string };
-  boundingClientRect?: TextRectangle;
-  checked?: boolean | undefined;
-  classNames?: string[];
-  clientHeight?: number;
-  clientLeft?: number;
-  clientTop?: number;
-  clientWidth?: number;
-  focused?: boolean;
-  id?: string;
-  innerText?: string;
-  namespaceURI?: string | null;
-  offsetHeight?: number;
-  offsetLeft?: number;
-  offsetTop?: number;
-  offsetWidth?: number;
-  selected?: boolean | undefined;
-  selectedIndex?: number | undefined;
-  scrollHeight?: number;
-  scrollLeft?: number;
-  scrollTop?: number;
-  scrollWidth?: number;
-  style?: { [prop: string]: string };
-  tagName?: string;
-  value?: string | undefined;
-  visible?: boolean;
-  hasClass?(className: string): boolean;
-  getStyleProperty?(propertyName: string): string;
-  getAttribute?(attributeName: string): string | null;
-  getBoundingClientRectProperty?(propertyName: string): number;
-  hasAttribute?(attributeName: string): boolean;
-
-
-}
-}
+    get childElementCount(): number {
+      throw new Error(`Not Implemented`);
+    },
+    get childNodeCount(): number {
+      throw new Error(`Not Implemented`);
+    },
+    get hasChildElements(): boolean {
+      throw new Error(`Not Implemented`);
+    },
+    get hasChildNodes(): boolean {
+      throw new Error(`Not Implemented`);
+    },
+    get nodeType(): number {
+      throw new Error(`Not Implemented`);
+    },
+    get textContent(): string {
+      throw new Error(`Not Implemented`);
+    },
+    get attributes(): { [name: string]: string } | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get boundingClientRect(): TextRectangle | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get checked(): boolean | undefined | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get classNames(): string[] | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get clientHeight(): number | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get clientLeft(): number | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get clientTop(): number | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get clientWidth(): number | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get focused(): boolean | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get id(): string | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get innerText(): string | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get namespaceURI(): string | null | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get offsetHeight(): number | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get offsetLeft(): number | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get offsetTop(): number | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get offsetWidth(): number | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get selected(): boolean | undefined | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get selectedIndex(): number | undefined | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get scrollHeight(): number | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get scrollLeft(): number | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get scrollTop(): number | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get scrollWidth(): number | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get style(): { [prop: string]: string } | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get tagName(): string | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get value(): string | undefined | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    get visible(): boolean | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    hasClass(className: string): boolean | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    getStyleProperty(propertyName: string): string | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    getAttribute(attributeName: string): string | null | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    getBoundingClientRectProperty(propertyName: string): number | undefined {
+      throw new Error(`Not Implemented`);
+    },
+    hasAttribute(attributeName: string): boolean | undefined {
+      throw new Error(`Not Implemented`);
+    },
+  };
+};
