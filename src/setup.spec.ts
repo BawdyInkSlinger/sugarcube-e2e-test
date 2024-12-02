6f5ed191f09b647358ecd5843556fc128cb19c5c
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2024-12-01 20:49:26 -0800

    Test fails for the right reasons

--- src/setup.spec.ts
+++ src/setup.spec.ts
@@ -1,41 +1,40 @@
-import { Selector, SugarcubeParser } from '.';
+import { Selector, SugarcubeParser, TestController } from '.';
 import './test-api/internal/monkey-patching/jsdom/strings';
 
 describe('Setup', () => {
-  describe(`resetState`, () => {
-    const init = {
-      title: 'StoryInit',
-      tags: [],
-      text: `
+  const init = {
+    title: 'StoryInit',
+    tags: [],
+    text: `
 <<set setup.init_s1_s2 = 'story init'>>
 <<set setup.init = 'story init'>>
         `,
-    };
+  };
 
-    const s1 = {
-      title: '_s1',
-      tags: ['script'],
-      text: `
+  const s1 = {
+    title: '_s1',
+    tags: ['script'],
+    text: `
 setup.init_s1_s2 = '_s1';
 setup.s1 = '_s1';
 setup.s1_s2 = 's1';
 `,
-    };
+  };
 
-    const s2 = {
-      title: 's2',
-      tags: ['script'],
-      text: `
+  const s2 = {
+    title: 's2',
+    tags: ['script'],
+    text: `
 setup.init_s1_s2 = 's2';
 setup.s1_s2 = 's2';
 setup.s2 = 's2';
 `,
-    };
+  };
 
-    const start = {
-      title: 'Start',
-      tags: [],
-      text: `
+  const start = {
+    title: 'Start',
+    tags: [],
+    text: `
 <<button "setup.start = 1" \`passage()\`>><<script>>setup.start = 1<</script>><</button>>
 
 setup.init_s1_s2: <span class="init_s1_s2"><<= setup.init_s1_s2>></span>
@@ -45,58 +44,73 @@ setup.s1_s2: <span class="s1_s2"><<= setup.s1_s2>></span>
 setup.start: <span class="start"><<= setup.start>></span>
 setup.init: <span class="init"><<= setup.init>></span>
 `,
-    };
+  };
 
-    it(`resets setup and recreates it correctly`, async () => {
-      const sugarcubeParser = await SugarcubeParser.create({
-        passages: [init, s1, s2, start],
-      });
+  it(`resets setup and recreates it correctly`, async () => {
+    const sugarcubeParser = await SugarcubeParser.create({
+      passages: [init, s1, s2, start],
+    });
+    const t = sugarcubeParser.testController;
 
-      await sugarcubeParser.testController
-        .goto('Start')
-        .expect(Selector(`.init_s1_s2`).innerText)
-        .eql('story init')
-        .expect(Selector(`.s1`).innerText)
-        .eql('_s1')
-        .expect(Selector(`.s2`).innerText)
-        .eql('s2')
-        .expect(Selector(`.s1_s2`).innerText)
-        .eql('s2')
-        .expect(Selector(`.start`).innerText)
-        .eql('[undefined]')
-        .expect(Selector(`.init`).innerText)
-        .eql('story init')
-        // click the button to initialize c
-        .click(Selector(`.passage button`).withText(`setup.start = 1`))
-        .expect(Selector(`.init_s1_s2`).innerText)
-        .eql('story init')
-        .expect(Selector(`.s1`).innerText)
-        .eql('_s1')
-        .expect(Selector(`.s2`).innerText)
-        .eql('s2')
-        .expect(Selector(`.s1_s2`).innerText)
-        .eql('s2')
-        .expect(Selector(`.start`).innerText)
-        .eql('1')
-        .expect(Selector(`.init`).innerText)
-        .eql('story init');
+    await t.goto('Start');
 
-      sugarcubeParser.resetState();
+    await assertValues(t, {
+      init_s1_s2: 'story init',
+      s1: '_s1',
+      s2: 's2',
+      s1_s2: 's2',
+      start: '[undefined]',
+      init: 'story init',
+    });
+    // click the button to initialize c
+    await t.click(Selector(`.passage button`).withText(`setup.start = 1`));
+    await assertValues(t, {
+      init_s1_s2: 'story init',
+      s1: '_s1',
+      s2: 's2',
+      s1_s2: 's2',
+      start: '1',
+      init: 'story init',
+    });
 
-      await sugarcubeParser.testController
-        .goto('Start')
-        .expect(Selector(`.init_s1_s2`).innerText)
-        .eql('story init')
-        .expect(Selector(`.s1`).innerText)
-        .eql('_s1')
-        .expect(Selector(`.s2`).innerText)
-        .eql('s2')
-        .expect(Selector(`.s1_s2`).innerText)
-        .eql('s2')
-        .expect(Selector(`.start`).innerText)
-        .eql('[undefined]')
-        .expect(Selector(`.init`).innerText)
-        .eql('story init');
+    sugarcubeParser.resetState();
+
+    await t.goto('Start');
+    await assertValues(t, {
+      init_s1_s2: 'story init',
+      s1: '_s1',
+      s2: 's2',
+      s1_s2: 's2',
+      start: '[undefined]',
+      init: 'story init',
     });
   });
+
+  type SetupValues = {
+    init_s1_s2: string;
+    s1: string;
+    s2: string;
+    s1_s2: string;
+    start: string;
+    init: string;
+  };
+
+  async function assertValues(
+    testController: TestController,
+    { init_s1_s2, s1, s2, s1_s2, start, init }: SetupValues
+  ) {
+    await testController
+      .expect(Selector(`.init_s1_s2`).innerText)
+      .eql(init_s1_s2)
+      .expect(Selector(`.s1`).innerText)
+      .eql(s1)
+      .expect(Selector(`.s2`).innerText)
+      .eql(s2)
+      .expect(Selector(`.s1_s2`).innerText)
+      .eql(s1_s2)
+      .expect(Selector(`.start`).innerText)
+      .eql(start)
+      .expect(Selector(`.init`).innerText)
+      .eql(init);
+  }
 });
